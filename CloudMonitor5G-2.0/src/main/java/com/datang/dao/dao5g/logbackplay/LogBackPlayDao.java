d6c7709cc3b7c3be2e58ff012134ba9219b87afa
xiaopan <xiaopan@datangmobile.cn>
2021-06-23 17:25:01 +0800

    增加顺序

--- CloudMonitor5G-2.0/src/main/java/com/datang/dao/dao5g/logbackplay/LogBackPlayDao.java
+++ CloudMonitor5G-2.0/src/main/java/com/datang/dao/dao5g/logbackplay/LogBackPlayDao.java
@@ -3,6 +3,7 @@ package com.datang.dao.dao5g.logbackplay;
 import com.datang.common.dao.GenericHibernateDao;
 import com.datang.domain.testLogItem.IEItem;
 import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,7 @@ public class LogBackPlayDao extends GenericHibernateDao<IEItem, Long> {
     public List<IEItem> getRecrodsByLogId(Long logId) {
         Criteria criteria = this.getHibernateSession().createCriteria(IEItem.class);
         criteria.add(Restrictions.eq("logId",logId));
+        criteria.addOrder(Order.asc("time"));
         List<IEItem> list = (List<IEItem>)criteria.list();
         return list;
     }
